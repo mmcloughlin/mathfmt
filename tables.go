67b01268b1b59bb1630906f3504cefd6181458a6
Michael McLoughlin <mmcloughlin@gmail.com>
2019-09-10 16:29:14 -0700

    \oplus symbol
    Extracted-from: mmcloughlin/ec3@c7b20761cd6982ab9ceb337905545a678c5ed370

--- tables.go
+++ tables.go
@@ -83,13 +83,14 @@ var chars = []Character{
 
 // symbols defines symbol replacements.
 var symbols = map[string]rune{
-	"+-":  '\u00B1', // PLUS-MINUS SIGN
-	"-+":  '\u2213', // MINUS-OR-PLUS SIGN
-	"==":  '\u2261', // IDENTICAL TO
-	"<=":  '\u2A7D', // LESS-THAN OR SLANTED EQUAL TO
-	">=":  '\u2A7E', // GREATER-THAN OR SLANTED EQUAL TO
-	"||":  '\u2225', // PARALLEL TO
-	"<-":  '\u2191', // LEFTWARDS ARROW
-	"->":  '\u2192', // RIGHTWARDS ARROW
-	"|->": '\u21a6', // RIGHTWARDS ARROW FROM BAR
+	"+-":      '\u00B1', // PLUS-MINUS SIGN
+	"-+":      '\u2213', // MINUS-OR-PLUS SIGN
+	"==":      '\u2261', // IDENTICAL TO
+	"<=":      '\u2A7D', // LESS-THAN OR SLANTED EQUAL TO
+	">=":      '\u2A7E', // GREATER-THAN OR SLANTED EQUAL TO
+	"||":      '\u2225', // PARALLEL TO
+	"<-":      '\u2191', // LEFTWARDS ARROW
+	"->":      '\u2192', // RIGHTWARDS ARROW
+	"|->":     '\u21a6', // RIGHTWARDS ARROW FROM BAR
+	"\\oplus": '\u2295', // CIRCLED PLUS
 }
