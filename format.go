8bc4dfa73f3f0d4dcfe4d0067fdf4187f1dceba4
Michael McLoughlin <mmcloughlin@gmail.com>
2020-02-04 23:14:24 -0800

    regexp-based solution
    Doesn't yet pass all tests, but wanted to save progress.

--- format.go
+++ format.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"go/ast"
 	"go/format"
 	"go/parser"
 	"go/token"
+	"regexp"
 	"strings"
-	"unicode"
-	"unicode/utf8"
 
 	"golang.org/x/tools/go/ast/astutil"
 )
@@ -24,17 +22,7 @@ func Format(src []byte) ([]byte, error) {
 	}
 
 	// Apply transform.
-	transformed := CommentTransform(f, func(text string) string {
-		newtext, errf := formula(text)
-		if errf != nil {
-			err = errf
-			return text
-		}
-		return newtext
-	})
-	if err != nil {
-		return nil, err
-	}
+	transformed := CommentTransform(f, formula)
 
 	// Format.
 	buf := bytes.NewBuffer(nil)
@@ -66,9 +54,16 @@ func CommentTransform(root ast.Node, transform func(string) string) ast.Node {
 
 // Fixed data structures required for formula processing.
 var (
-	replacer *strings.Replacer // replacer for symbols.
-	super    = map[rune]rune{} // replacement map for superscript characters.
-	sub      = map[rune]rune{} // replacement map for subscript characters.
+	// Symbol replacer.
+	replacer *strings.Replacer
+
+	// Regular expressions for super/subscripts.
+	supregexp *regexp.Regexp
+	subregexp *regexp.Regexp
+
+	// Rune replacement maps.
+	super = map[rune]rune{}
+	sub   = map[rune]rune{}
 )
 
 func init() {
@@ -79,151 +74,53 @@ func init() {
 	}
 	replacer = strings.NewReplacer(oldnew...)
 
-	// Build super/subscript replacement maps.
+	// Build super/subscript character classes and replacement maps.
+	var superclass, subclass []rune
 	for _, char := range chars {
 		if char.Super != None {
+			superclass = append(superclass, char.Char)
 			super[char.Char] = char.Super
 		}
 		if char.Sub != None {
+			subclass = append(subclass, char.Char)
 			sub[char.Char] = char.Sub
 		}
 	}
+
+	// Build regular expressions.
+	supregexp = regexp.MustCompile(`(\b[A-Za-z0-9]|\pS)\^(\d+|\{` + charclass(superclass) + `+\})`)
+	subregexp = regexp.MustCompile(`(\b[A-Za-z]|\pS)_(\d+\b|\{` + charclass(subclass) + `+\})`)
+}
+
+// charclass builds a regular expression character class from a list of runes.
+func charclass(runes []rune) string {
+	return strings.ReplaceAll("["+string(runes)+"]", "-", `\-`)
 }
 
 // formula processes a formula in s, writing the result to w.
-func formula(s string) (string, error) {
-	if len(s) == 0 {
-		return "", nil
-	}
-
+func formula(s string) string {
 	// Replace symbols.
 	s = replacer.Replace(s)
 
-	// Replace super/subscripts.
-	buf := bytes.NewBuffer(nil)
-	last := None
-	for len(s) > 0 {
-		r, size := utf8.DecodeRuneInString(s)
+	// Replace superscripts.
+	s = supregexp.ReplaceAllStringFunc(s, subsupreplacer(super))
 
-		// Look for a super/subscript character.
-		var repl map[rune]rune
-		switch r {
-		case '^':
-			repl = super
-		case '_':
-			repl = sub
-		default:
-			buf.WriteRune(r)
-			last = r
-			s = s[size:]
-			continue
-		}
+	// Replace subscripts.
+	s = subregexp.ReplaceAllStringFunc(s, subsupreplacer(sub))
 
-		// Perform replacement.
-		if unicode.IsPrint(last) && !unicode.IsSpace(last) {
-			var err error
-			s, err = supsub(buf, s, repl)
-			if err != nil {
-				return "", err
+	return s
+}
+
+func subsupreplacer(repl map[rune]rune) func(string) string {
+	return func(s string) string {
+		var runes []rune
+		for i, r := range s {
+			if i == 0 {
+				runes = append(runes, r)
+			} else if repl[r] != None {
+				runes = append(runes, repl[r])
 			}
-		} else {
-			buf.WriteRune(r)
-			s = s[size:]
 		}
-
-		last = None
+		return string(runes)
 	}
-
-	return buf.String(), nil
-}
-
-// supsub processes a super/subscript starting at s, writing the result to w.
-// The repl map provides the mapping from runes to the corresponding
-// super/subscripted versions. Note the first character of s should be the "^"
-// or "_" operator.
-func supsub(w *bytes.Buffer, s string, repl map[rune]rune) (string, error) {
-	arg, rest, err := parsearg(s[1:])
-	if err != nil {
-		return "", err
-	}
-
-	// If we could not parse an argument, or its not replaceable, just write the
-	// sub/script operator and return.
-	if len(arg) == 0 || !replaceable(arg, repl) {
-		w.WriteByte(s[0])
-		return s[1:], nil
-	}
-
-	// Perform the replacement.
-	for _, r := range arg {
-		w.WriteRune(repl[r])
-	}
-
-	return rest, nil
-}
-
-// parsearg parses the argument to a super/subscript.
-func parsearg(s string) (string, string, error) {
-	if len(s) == 0 {
-		return "", "", nil
-	}
-
-	// Braced.
-	if s[0] == '{' {
-		arg, rest, err := parsebraces(s)
-		if err != nil {
-			return "", "", err
-		}
-		return arg[1 : len(arg)-1], rest, nil
-	}
-
-	// Look for a numeral.
-	i := 0
-	for ; i < len(s) && '0' <= s[i] && s[i] <= '9'; i++ {
-	}
-	if i > 0 {
-		return s[:i], s[i:], nil
-	}
-
-	// Default to the first rune.
-	_, i = utf8.DecodeRuneInString(s)
-	return s[:i], s[i:], nil
-}
-
-// parsebraces parses matching braces starting at the beginning of s.
-func parsebraces(s string) (string, string, error) {
-	if len(s) == 0 || s[0] != '{' {
-		return "", "", errors.New("expected {")
-	}
-
-	depth := 0
-	for i, r := range s {
-		// Adjust depth if we see open or close brace.
-		switch r {
-		case '{':
-			depth++
-		case '}':
-			depth--
-		}
-
-		// Continue if we have not reached matched braces.
-		if depth > 0 {
-			continue
-		}
-
-		// Return the matched braces.
-		return s[:i+1], s[i+1:], nil
-	}
-
-	return "", "", errors.New("unmatched braces")
-}
-
-// replaceable returns whether every rune in s has a replacement in repl.
-func replaceable(s string, repl map[rune]rune) bool {
-	for _, r := range s {
-		if _, ok := repl[r]; !ok {
-			return false
-		}
-	}
-	return true
 }
